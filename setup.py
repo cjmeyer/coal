7c791f43d5f13a9d3674bb022b3189d423ba7621
Corrin Meyer <corrin.meyer@dornerworks.com>
2011-05-04 15:28:54 -0400

    Fixed setup dependency

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="coal",
     version="0.1.0",
     test_suite="unittest2.collector",
-    tests_require=["unittest2", "mock"],
-    install_requires=[],
+    tests_require=["unittest2"],
+    install_requires=["mock"],
     packages=find_packages(exclude=[])
 )
 
