f169d215354261f9a9167518883b35a708b47eca
Low Kian Seong <kianseong@gmail.com>
2014-09-23 10:38:03 +0800

    Update setup.py
    Adding more metadata to `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="coal",
     version="0.1.1",
+    description="Console application library in Python",
+    url="https://github.com/cjmeyer/coal",
     test_suite="unittest2.collector",
     tests_require=["unittest2"],
     install_requires=["Mako"],
